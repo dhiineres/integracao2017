4460fb7d95e594fc438a607de6722b2764e41d79
Dhiineres <dhiogoneres@gmail.com>
2017-11-13 11:17:06 -0200

    adicionados resumos disciplinas 7º, 8º e 9º períodos

--- docs/tipuesearch/tipuesearch_content.js
+++ docs/tipuesearch/tipuesearch_content.js
@@ -66,7 +66,79 @@ var tipuesearch = {"pages": [
         "text": "The CSS z-index property often trips up new and even experienced developers. The aim of this article is to boil down a somewhat-complex specification to 3 major points, which should ease most z-index pain.", 
         "tags": "", 
         "url": "http://www.tipue.com/z-index"
-    }
+    },
+	{
+		"title": "Software Concorrente e Distribuído",
+		"text": "Qualidade de produto (4h): escalabilidade, tolerância a falhas, disponibilidade, desempenho.\n Conceitos (4h): middleware, distribuição, paralelismo, concorrência, RESTful, web, internet das coisas, contêineres, troca de mensagens, micro-serviços, serviços web.",
+		"tags": "Núcleo NC Período Sétimo Unidade INF Obrigatória Pré-requisito Nenhum",
+		"url": ""
+	},
+	{
+		"title": "Experiência do Usuário de Software",
+		"text":"Qualidade em uso (4h): usabilidade, acessibilidade. Design centrado no usuário (8h): diretrizes, pesquisa, personas, jornadas de usuário, modelagem (especificação) e ferramentas. Projeto de interação (16h): diretrizes, criação, modelagem (especificação) e ferramentas.",
+		"tags": "Núcleo NC Período Sétimo Unidade INF Obrigatória Pré-requisito Nenhum",
+		"url": ""
+	},
+	{
+		"title": "Arquitetura de Software",
+		"text": "Conceitos (4h): software, qualidade de software, requisitos de software, arquitetura de software e projeto detalhado. Requisitos funcionais e requisitos de qualidade (8h): Conceituação e identificação de requisitos relevantes para a arquitetura.",
+		"tags": "Núcleo NC Período Sétimo Unidade INF Obrigatória Pré-requisito Nenhum",
+		"url": ""
+	},
+	{
+		"title": "Requisitos de Software",
+		"text": "Processo de requisitos (16h): definições básicas, eliciação de requisitos, análise de requisitos, especificação de requisitos, verificação e validação de requisitos. Modelos e métodos da engenharia de requisitos (32).",
+		"tags": "Núcleo NC Período Sétimo Unidade INF Obrigatória Pré-requisito Nenhum",
+		"url": ""
+	},
+	{
+		"title": "Governança e Gestão de Serviços de Software",
+		"text": "Governança (8h): governança corporativa; governança de TI (GTI); governança de Software (GS); princípios da GS; conformidade (compliance); alinhamento dos serviços de software ao negócio da organização;",
+		"tags": "Núcleo NC Período Sétimo Unidade INF Obrigatória Pré-requisito Nenhum",
+		"url": ""
+	},
+	{
+		"title": "Software para Computação Ubíqua",
+		"text": "Sistemas de informação que fazem uso de dispositivos (ubíquos) (16h): smartphones, sensores, internet das coisas (IoT), stream analytics e aspectos de segurança (vulnerabilidades, criptografia, certificados digitais). Definição de arquiteturas para soluções móveis (16): conectar serviços, possivelmente de grande volume, fluxo e em tempo real, com a necessidade de analisá-los.",
+		"tags": "Núcleo NC Período Oitavo Unidade INF Obrigatória Pré-requisito Nenhum",
+		"url": ""
+	},
+	{
+		"title": "Software para Persistência de Dados",
+		"text": "Visão geral de persistência (12h): arquivos em formato texto/binário. Serialização de objetos. Documentos XML. JSON. Bancos de dados relacionais. Bancos NoSql. Modelagem e implementação de bancos de dados (28h). Mapeamento entre OO e relacional (8h). Refatoração de bancos de dados (8h).",
+		"tags": "Núcleo NC Período Oitavo Unidade INF Obrigatória Pré-requisito Nenhum",
+		"url": ""
+	},
+	{
+		"title": "Padrões de Arquitetura de Software",
+		"text": "Conceitos (12h): arquitetura de software e padrões de arquitetura (blackboard, microservices, invocação implícita, arquitetura orientada a serviços, multitier, arquitetura orienta a eventos, plugin, filtros e pipes, MVC. Norma ISO/IEC/IEEE 42010:2011 (8h). Análise (avaliação) de arquiteturas de software (12h). Criação de arquiteturas de software (32h).",
+		"tags": "Núcleo NC Período Oitavo Unidade INF Obrigatória Pré-requisito Nenhum",
+		"url": ""
+	},
+	{
+		"title": "Teste de Software",
+		"text": "Processo de construção (8h): definições básicas, atividades e documentação. Processo de Teste de Software (56): definições básicas, técnicas de teste, teste baseado em intuição e experiência do engenheiro de software, atividades do processo, documentação e ferramentas.",
+		"tags": "Núcleo NC Período Oitavo Unidade INF Obrigatória Pré-requisito Nenhum",
+		"url": ""
+	},
+	{
+		"title":"Metodologia e Experimentação em Engenharia de Software",
+		"text":"Metodologia Científica (16h): aplicação de metodologia científica em atividades de Engenharia de Software. Fundamentos da Engenharia de Software Experimental (16h): contexto da experimentação na Engenharia de Software;",
+		"tags":"Núcleo NC Período Oitavo Unidade INF Optativa Pré-requisito Nenhum",
+		"url":""
+	},
+	{
+		"title":"Mercado e Economia de Software",
+		"text":"Modelos de negócio para software (aluguel, serviço, código aberto). Leis, normas, impostos e legislação brasileira para o mercado local e para a exportação de software. Programas de incentivo à exportação e à produção de software. Características e exigências do mercado interno e externo. Identificação de oportunidades de inovação em software. Planos de negócio de software para o mercado nacional e global.",
+		"tags":"Núcleo NC Período Oitavo Unidade INF Optativa Pré-requisito Nenhum",
+		"url":""
+	},
+	{
+		"title": "Prática em Engenharia de Software",
+		"text": "O egresso do BES passa pela vivência de dois tipos de experiências práticas diferentes em suas essências. O primeiro grupo de experiências é composto pelas experiências promovidas nas atividades práticas realizadas de maneira contínua e integrada desde o início do curso...",
+		"tags": "Núcleo NC Período Nono Unidade INF Obrigatória Pré-requisito Nenhum",
+		"url": ""
+	}
 ]};
 
 
