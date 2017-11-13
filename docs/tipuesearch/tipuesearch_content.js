fdb245fb27254bfc30910ddfc047cce7dbbea162
Rodrigo Aguiar <rodrigoaguiar0@gmail.com>
2017-11-13 10:09:53 -0200

    Atualização das disciplinas 1, 2 e 3

--- docs/tipuesearch/tipuesearch_content.js
+++ docs/tipuesearch/tipuesearch_content.js
@@ -66,6 +66,102 @@ var tipuesearch = {"pages": [
         "text": "The CSS z-index property often trips up new and even experienced developers. The aim of this article is to boil down a somewhat-complex specification to 3 major points, which should ease most z-index pain.", 
         "tags": "", 
         "url": "http://www.tipue.com/z-index"
+    },
+    {
+        "title": "Computação e Sociedade",
+        "text": "História da computação. Estudo e análise de casos de aplicação de computadores na sociedade e para o meio ambiente. Subáreas da computação e áreas interdisciplinares. Importância e desafios da computação no Brasil e no mundo. Cursos de computação e aspectos profissionais: tipos de cursos, perfis profissionais, demanda do mercado, organizações e associações na área, regulamentação da profissão. Leis e normas relacionadas à Informática. Questões ambientais, raciais, de saúde e de inclusão digital relacionadas à Computação. Ética na Computação. Empresas de tecnologia da informação. Incubadoras de empresas.",
+        "tags": "Gestão de Engenharia de Software Prática do profissional de engenharia de software Núcleo NC Período Primeiro Unidade INF Obrigatória Pré-requisito Nenhum",
+        "url": ""
+    },
+    {
+        "title": "Introdução à Programação",
+        "text": "Introdução a algoritmos. Conceitos básicos de programas: constantes; tipos de dados primitivos; variáveis; atribuição; entrada e saída de dados; expressões; estruturas de decisão; estruturas de repetição. Ponteiro. Estruturas de dados homogêneas e heterogêneas: vetores, matrizes, cadeias de caracteres, registros. Subprogramas: funções; passagens de parâmetros por valor e por referência, recursividade. Manipulação de arquivos: abertura, fechamento, leitura e gravação. Tipos de acesso a arquivos: sequencial e indexado. Tipos de arquivos (texto e binário). Transcrição de algoritmos para uma linguagem de programação. Domínio de uma linguagem de programação: sintaxe e semântica; interpretação e compilação de programas; ambiente de desenvolvimento de programas; estilo de codificação; documentação de código; técnicas de depuração e técnicas de profiling; desenvolvimento e uso de bibliotecas.",
+        "tags": "Construção de Software Fundamentos Computacionais Núcleo NC Período Primeiro Unidade INF Obrigatória Pré-requisito Nenhum",
+        "url": ""
+    },
+    {
+        "title": "Cálculo 1A",
+        "text": "Números reais. Funções reais de uma variável real e suas inversas. Noções sobre cônicas. Limite e continuidade. Derivadas e aplicações. Polinômio de Taylor. Integrais. Técnicas de Integração. Integrais impróprias. Aplicações.",
+        "tags": "Fundamentos Matemáticos Núcleo NC Período Primeiro Unidade IME Obrigatória Pré-requisito Nenhum",
+        "url": ""
+    },
+    {
+        "title": "Fundamentos de Matemática para Computação",
+        "text": "Noções de lógica. Introdução a demonstrações. Indução matemática, Recursividade e Relações de Recorrência, Conjuntos e Combinatória. Séries e sequências. Relações e Funções. Representações numéricas e Mudança de base numérica.",
+        "tags": "Fundamentos Matemáticos Fundamentos Computacionais Núcleo NC Período Primeiro Unidade INF Obrigatória Pré-requisito Nenhum",
+        "url": ""
+    },
+    {
+        "title": "Arquitetura de computadores",
+        "text": "Visão geral dos computadores modernos. Evolução da arquitetura dos computadores. Memória e representação de dados e instruções. Processador, ciclo de instrução, formato e endereçamento. Noções básicas de programação em linguagem de montagem.Dispositivos de entrada e saída.Sistemas de interconexão (barramentos). Interfaceamento e técnicas de entrada e saída. Hierarquia de memória. Introdução a arquiteturas paralelas e métricas de desempenho.",
+        "tags": "Fundamentos Computacionais Fundamentos de Engenharia Núcleo NC Período Segundo Unidade INF Obrigatória Pré-requisito Nenhum",
+        "url": ""
+    },
+    {
+        "title": "Algoritmos e Estruturas de Dados 1",
+        "text": "Noções de complexidade de algoritmos (notações de complexidade). Algoritmos de pesquisa: pesquisa sequencial e binária. Algoritmos de ordenação. Tipos abstratos de dados. Estruturas de dados utilizando vetores: pilhas, filas, listas (simples e circulares). Estruturas de dados com alocação dinâmica de memória: pilhas, filas, listas (simplesmente encadeadas, duplamente encadeadas e circulares).",
+        "tags": "Construção de Software Fundamentos Computacionais Fundamentos Matemáticos Fundamentos de Engenharia Núcleo NC Período Segundo Unidade INF Obrigatória Pré-requisito Introdução à Programação",
+        "url": ""
+    },
+    {
+        "title": "Probabilidade e Estatísticas A",
+        "text": "Estatística descritiva. Noções sobre amostragem. Introdução à teoria de conjuntos. Introdução à teoria de probabilidade: espaço amostral, eventos, frequência relativa, fundamentos de probabilidade, probabilidade condicional, eventos independentes e teorema de Bayes. Variáveis aleatórias: conceitos básicos, esperança e variância. Distribuições discretas de probabilidade: uniforme, binomial e Poisson. Distribuições contínuas de probabilidade: uniforme, exponencial, normal e t-Student. Estimação pontual e intervalar para uma população: média e proporção. Teste de hipóteses para uma população: média e proporção. Correlação linear e regressão linear simples.",
+        "tags": "Fundamentos Matemáticos Núcleo NC Período Segundo Unidade IME Obrigatória Pré-requisito Cálculo 1A",
+        "url": ""
+    },
+    {
+        "title": "Álgera linear",
+        "text": "Sistemas lineares e matrizes. Espaços vetoriais. Transformações lineares. Autovalores e autovetores. Espaços com produto interno.",
+        "tags": "Fundamentos Matemáticos Núcleo NC Período Segundo Unidade IME Obrigatória Pré-requisito Nenhum",
+        "url": ""
+    },
+    {
+        "title": "Lógica Matemática",
+        "text": "Noções básicas: linguagem natural vs linguagens formais; verdade, validade, satisfatibilidade; lógica proposicional (sintaxe e semântica, propriedades e relações semânticas, consequência lógica, simplificação de fórmulas); lógica de primeira ordem (sintaxe e semântica, propriedades e relações semânticas, formas normais). Métodos de validação: métodos diretos de prova; métodos de prova por contradição; indução estrutural. Linguagem para experimentação. Aplicações básicas.",
+        "tags": "Fundamentos Computacionais Fundamentos Matemáticos Núcleo NC Período Segundo Unidade IME Obrigatória Pré-requisito Fundamentos de Matemática para Computação",
+        "url": ""
+    },
+    {
+        "title": "Programação Orientada a Objetos",
+        "text": "Abstração e tipos abstratos de dados. Classes, métodos, encapsulamento, interface. Mensagens, instâncias e inicialização. Herança e composição. Polimorfismo. Uso de uma linguagem orientada a objetos. Noções de UML. Noções de padrões de projeto orientado a objetos. ",
+        "tags": "Construção de Software Modelos e Métodos de Engenharia de Software Fundamentos Computacionais Fundamentos de Engenharia Núcleo NC Período Terceiro Unidade INF Obrigatória Pré-requisito Introdução à Programação",
+        "url": ""
+    },
+    {
+        "title": "Algoritmos e Estruturas de Dados 2",
+        "text": "Árvores: formas de representação, recursão em árvores, árvores binárias, árvores binárias de busca, árvores balanceadas (AVL e rubro-negras). Filas de prioridades. Heaps, Heapsort.  Hashing: tipos de funções de hashing; tratamento de colisões. Definições de Grafos. Estruturas de Dados para representação de grafos. Algoritmos básicos em grafos.",
+        "tags": "Construção de Software Fundamentos Computacionais Fundamentos Matemáticos Fundamentos de Engenharia Núcleo NC Período Terceiro Unidade INF Obrigatória Pré-requisito Algoritmos e Estrutura de Dados 1",
+        "url": ""
+    },
+    {
+        "title": "Linguagens e Paradigmas de Programação",
+        "text": "Estudo dos conceitos de linguagens de programação e dos paradigmas de programação: procedural, funcional, lógico, orientado a objeto e script. Reflexão sobre as características desejáveis em uma linguagem de programação e os critérios de seleção de linguagens de acordo com as especificidades dos domínios de aplicação. Descrição de sintaxe e semântica. Estudo sobre tipos de dados, estruturas de controle, ambientes de execução, variáveis, expressões e subprogramas em linguagens de programação.",
+        "tags": "Construção de Software Modelos e Métodos de Engenharia de Software Fundamentos Computacionais Fundamentos Matemáticos Núcleo NC Período Terceiro Unidade INF Obrigatória Pré-requisito Introdução à Programação",
+        "url": ""
+    },
+    {
+        "title": "Engenharia de Software",
+        "text": "Requisitos de software. Projeto (design) de software. Construção de software. Teste de software. Manutenção de software. Gerência de configuração de software. Gerência de projeto de software. Processo de engenharia de software. Modelos e métodos de engenharia de software. Qualidade de software. Prática profissional de engenharia de software. Economia para engenharia de software. Fundamentos de engenharia.",
+        "tags": "Requisitos de Software Construção de Software Processo de Engenharia de Software Modelos e Métodos de Engenharia de Software Fundamentos de Engenharia Núcleo NC Período Terceiro Unidade INF Obrigatória Pré-requisito Nenhum",
+        "url": ""
+    },
+    {
+        "title": "Pesquisa Operacional",
+        "text": "Modelagem. Problema de Programação Linear (PL). Resolução gráfica de PL. Algoritmo Simplex. Dualidade. Algoritmo Simplex-Dual. Pós-otimização e Análise de Sensibilidade.",
+        "tags": "Fundamentos Computacionais Fundamentos Matemáticos Fundamentos de Engenharia Núcleo NE Período Terceiro Unidade INF Optativa Pré-requisito Álgebra Linear",
+        "url": ""
+    },
+    {
+        "title": "Linguagens Formais e Autômatos",
+        "text": "Conceitos básicos de linguagens. Mecanismos geradores (gramáticas) e reconhecedores (determinísticos e não determinísticos) de linguagens regulares, livres de contexto e sensíveis ao contexto; relação entre estas classes de linguagens e suas principais propriedades. Hierarquia de Chomsky.",
+        "tags": "Fundamentos Computacionais Fundamentos Matemáticos Fundamentos de Engenharia Núcleo NE Período Terceiro Unidade INF Optativa Pré-requisito Fundamentos da Matemática para Computação",
+        "url": ""
+    },
+    {
+        "title": "Sistemas Operacionais",
+        "text": "Conceitos de Hardware e Software. Tipos de Sistemas Operacionais. Organização da Estrutura Interna do Sistema Operacional. Gerência de Processos. Gerência do Processador. Gerência de Memória. Gerência de Dispositivos de Entrada e Saída. Sistemas de Arquivos. Estudos de casos de sistemas operacionais atuais.",
+        "tags": "Fundamentos Computacionais Núcleo NE Período Terceiro Unidade INF Optativa Pré-requisito Arquitetura de Computadores",
+        "url": ""
     }
 ]};
 
